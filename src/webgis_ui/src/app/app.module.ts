c50c379eeaaeab78861230224d2bfd9dee00bd64
PheerawatB <pheerawat.b@riseplus.tech>
2024-09-24 10:48:11 +0700

    + move layout to level1 + change name menu to navigation

--- src/webgis_ui/src/app/app.module.ts
+++ src/webgis_ui/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './menu/side-bar/side-bar.component';
-import { TopBarComponent } from './menu/top-bar/top-bar.component';
+import { SideBarComponent } from './navigation/side-bar/side-bar.component';
+import { TopBarComponent } from './navigation/top-bar/top-bar.component';
 import { EditorMapComponent } from './page/editor-map/editor-map.component';
 import { LiveMonitorComponent } from './page/live-monitor/live-monitor.component';
-import { LayoutComponent } from './menu/layout/layout.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
