3509fa840ded5bea80eac0c99bb919c35a14f9e6
PheerawatB <pheerawat.b@riseplus.tech>
2024-10-03 16:21:02 +0700

    chart

--- src/webgis_ui/src/app/navigation/sensor-info/footer/footer.component.ts
+++ src/webgis_ui/src/app/navigation/sensor-info/footer/footer.component.ts
@@ -1,25 +1,61 @@
-import { Component } from '@angular/core';
-import { ChartOptions, ChartData, ChartType } from 'chart.js';
+import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartData, ChartType, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, BarController } from 'chart.js';
+import { Chart } from 'chart.js';
+Chart.register(CategoryScale, LinearScale, BarElement,BarController, Title, Tooltip, Legend);
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
-  public barChartOptions: ChartOptions = {
-    responsive: true,
-  };
 
-  public barChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+export class FooterComponent implements OnInit {
+  
+  barChart : any = []
+  ngOnInit(): void {
+    const currentDate = new Date();
+    const month = currentDate.getMonth() + 1; // Get the current month (1-based)
+    const year = currentDate.getFullYear();   // Get the current year
+    const daysInMonth = this.getDaysInMonth(month, year);
 
-  public barChartData: ChartData<'bar'> = {
-    labels: this.barChartLabels,
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-    ]
-  };
+    const dataForMonth = Array.from({ length: daysInMonth.length }, () => Math.floor(Math.random() * 100)); // Random data for each day
+    this.barChart = new Chart('barChart', {
+      type: "bar",
+      data: {
+        labels: daysInMonth,
+        datasets: [{
+          label: '',
+          data: dataForMonth,
+          backgroundColor: '#00FCFF',
+          borderColor: '#00FCFF',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            beginAtZero: true
+          },
+          y: {
+            beginAtZero: true
+          }
+        },
+        // plugins: {
+        //   legend: {
+        //     display: true,
+        //     position: 'top' // Position of the chart legend
+        //   },
+        //   tooltip: {
+        //     enabled: true
+        //   }
+        // }
+      }
+    });
+  }
 
-  public barChartType: ChartType = 'bar';
+  getDaysInMonth(month: number, year: number): string[] {
+    const days = new Date(year, month, 0).getDate();
+    return Array.from({ length: days }, (_, i) => (i + 1).toString()); // Generate array of days as strings
+  }
 }
